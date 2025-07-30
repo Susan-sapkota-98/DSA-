6e7b84fd7bc0518863dc069de8e515d7aa754925
susan-sapkota-98 <susansapkota986@gmail.com>
2025-07-30 09:15:24 +0545

    New file added

---
+++ sorting/Heap_sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void maxHeapify(int A[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i;
+    int r = 2 * i + 1;
+
+    if (l <= n && A[l] > A[largest])
+        largest = l;
+    if (r <= n && A[r] > A[largest])
+        largest = r;
+
+    if (largest != i)
+    {
+        swap(&A[i], &A[largest]);
+        maxHeapify(A, n, largest);
+    }
+}
+
+void heapSort(int A[], int n)
+{
+    // Build max heap
+    for (int i = n / 2; i >= 1; i--)
+    {
+        maxHeapify(A, n, i);
+    }
+
+    // Extract elements from heap
+    for (int i = n; i >= 2; i--)
+    {
+        swap(&A[1], &A[i]);
+        maxHeapify(A, i - 1, 1);
+    }
+}
+
+int main()
+{
+    int A[] = {0, 4, 10, 3, 5, 1}; // 1-based index; A[0] is dummy
+    int n = 5;
+
+    heapSort(A, n);
+
+    printf("Sorted array: ");
+    for (int i = 1; i <= n; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+
+    return 0;
+}
