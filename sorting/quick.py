cb56960133373e237e1a28c4155d17c33ea0d7f8
susan-sapkota-98 <susansapkota986@gmail.com>
2025-08-01 17:59:29 +0545

    small change

--- sorting/quick.py
+++ sorting/quick.py
@@ -1,25 +1,28 @@
+# A=array l=low , r=high , pivot=p
 def quicksort(A,l,r):
-    if(l,r):
+    if l<r:
         p=partition(A,l,r)
         quicksort(A,l,p-1)
         quicksort(A,p+1,r)
 
 def partition(A,l,r):
-    x=l
-    y=r
-    pivot=A[l]
-    while(x<y):
-        while(A[x]<=pivot):
-            x++
-        while(A[y]>pivot):
-            y--
-        if(x<y):
-            swap(A[x],A[y])
+    p=A[l]
+    i=l+1
+    j=r
+    while True:
+        while i<=j and A[i]<=p:
+            i+=1
+        while i<=j and A[j]>=p:
+            j-=1
+        if i<=j:
+            A[i],A[j]=A[j],A[i]
+        else:
+            break
+    A[l],A[j]=A[j],A[l]
+    return j
+    
 
-def swap(A[l],A[y]):
-    return y
-
-data_list = list(map(int, input("Enter numbers separated by space: ").split()))
-
-result = quicksort(data_list)
-print("Sorted list:", result)
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+print("The list of array before sorting:", nums)
+quicksort(nums,0,len(nums)-1)
+print("Sorted list:", nums)
