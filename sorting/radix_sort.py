49c8e04713b8c4499542de001d2c6d448a5c2d71
susan-sapkota-98 <susansapkota986@gmail.com>
2025-08-02 16:59:23 +0545

    small change

--- sorting/radix_sort.py
+++ sorting/radix_sort.py
@@ -1,38 +1,35 @@
-def get_max(arr):
-    return max(arr)
-
 def count_sort(arr, pos):
     n = len(arr)
     output = [0] * n
     count = [0] * 10
 
-    # Count occurrences of digits at the given position
-    for i in range(n):
-        index = (arr[i] // pos) % 10
-        count[index] += 1
+    for i in range(0,n):
+        temp= (arr[i] // pos) % 10
+        count[temp] += 1
 
     for i in range(1, 10):
         count[i] += count[i - 1]
         
     i=n-1
     while i>=0:
-        digit=(arr[i]//pos)%10
-        output[count[digit]-1]=arr[i]
-        count[digit] -= 1
-        i -=1
+        temp=(arr[i]//pos)%10
+        output[count[temp]-1]=arr[i]
+        count[temp] -= 1
+        i =i-1
 
-    
-    for i in range(n):
+    for i in range(0,n):
         arr[i] = output[i]
 
 def radix_sort(arr):
-    max_val = get_max(arr)
+    max_val = max(arr)
+    #for unit place pos=1
     pos = 1
     while max_val // pos > 0:
         count_sort(arr, pos)
         pos *= 10
 
 
-arr = [15,17,7,2,1,7,3,11]
+arr = [157,15,7,20,1,72,333,111]
+print("The original array is:", arr)
 radix_sort(arr)
-print(" The Sorted array is:", arr)
+print("The Sorted array is:", arr)
