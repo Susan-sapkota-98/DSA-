b23baad8e7e9e80ab701cbc653174b6d3fd16c90
susan-sapkota-98 <susansapkota986@gmail.com>
2025-08-02 16:58:38 +0545

    small change

--- sorting/Radix_sort.c
+++ sorting/Radix_sort.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to get maximum value in the array
 int get_max(int A[], int n)
 {
     int max = A[0];
@@ -13,33 +12,27 @@ int get_max(int A[], int n)
     return max;
 }
 
-// Counting Sort used by Radix Sort
 void count_sort(int A[], int n, int pos)
 {
-    int output[n];       // Output array
-    int count[10] = {0}; // For digits 0-9
+    int output[n];       
+    int count[10] = {0};
 
-    // Count occurrences of each digit at 'pos'
     for (int i = 0; i < n; i++)
         count[(A[i] / pos) % 10]++;
 
-    // Cumulative count
     for (int i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
-    // Build the output array (stable sort)
     for (int i = n - 1; i >= 0; i--)
     {
         int digit = (A[i] / pos) % 10;
         output[--count[digit]] = A[i];
     }
 
-    // Copy output to original array
     for (int i = 0; i < n; i++)
         A[i] = output[i];
 }
 
-// Main Radix Sort function
 void radix_sort(int A[], int n)
 {
     int max = get_max(A, n);
@@ -47,7 +40,6 @@ void radix_sort(int A[], int n)
         count_sort(A, n, pos);
 }
 
-// Driver code
 int main()
 {
     int A[] = {170, 45, 75, 90, 802, 24, 2, 66};
